ca2b3d77b7edae079e78a8459d4cc76057c73b91
MrDonex <sylenko.matvii@icloud.com>
2025-12-10 18:23:41 +0200

    css

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const keyEL = document.querySelector('.key-back')
+const stateEL = document.querySelector('.state')
+
+const keys = ['k', 'u', 'f', 'h', 'j', 'd', 'x', 'q', 'e', 'y']
+
+let counterKeys = 0;
