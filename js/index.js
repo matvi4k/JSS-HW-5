ec0a934e1b58b786e8436844d7784aec3ebb9bbb
MrDonex <sylenko.matvii@icloud.com>
2025-12-15 19:03:58 +0200

    oh yes

--- js/index.js
+++ js/index.js
@@ -1,6 +1,44 @@
-const keyEL = document.querySelector('.key-back')
-const stateEL = document.querySelector('.state')
+const keyEL = document.querySelector("#key");
+const stateEL = document.querySelector(".state");
+const btnEl = document.querySelector(".js-start-game");
 
-const keys = ['k', 'u', 'f', 'h', 'j', 'd', 'x', 'q', 'e', 'y']
+const keys = ["k", "u", "f", "h", "j", "d", "x", "q", "e", "y"];
 
-let counterKeys = 0;
+let currentKeyIndex = -1;
+
+// ФУНКЦІЇ
+
+function startNewGame() {
+  currentKeyIndex = Math.floor(Math.random() * keys.length);
+  keyEL.textContent = `Натисни клавішу: ${keys[currentKeyIndex]}`;
+  alert("Розпочалася нова гра");
+  keyEL.style.marginLeft = '550px'
+
+  btnEl.disabled = true;
+}
+
+function onKeydownPress(event) {
+  const pressedKey = event.key.toLowerCase();
+  const currentKey = keys[currentKeyIndex];
+
+  if(pressedKey === currentKey){
+    currentKeyIndex += 1
+
+    if(currentKeyIndex === keys.length){
+        stateEL.textContent = 'Гру завершино'
+        btnEl.disabled = false;
+        currentKeyIndex = -1;
+        return
+    }
+
+    keyEL.textContent = `Натисни клавішу: ${keys[currentKeyIndex]}`;
+
+    stateEL.textContent = "Правильно!";  
+  } else{
+    stateEL.textContent = `Неправельно! Натисни клавішу ${keys[currentKeyIndex]}`;  
+  }
+}
+// ЛІСЕНЕРИ
+
+btnEl.addEventListener("click", startNewGame);
+document.addEventListener('keydown', onKeydownPress)
