ed30cbae3d44a5891d53a304fe1792c652ea3e0c
MrDonex <sylenko.matvii@icloud.com>
2025-12-17 16:30:25 +0200

    complited first game

--- js/index.js
+++ js/index.js
@@ -5,41 +5,44 @@ const btnEl = document.querySelector(".js-start-game");
 const keys = ["k", "u", "f", "h", "j", "d", "x", "q", "e", "y"];
 
 let currentKeyIndex = 0;
+let gameStarted = false;
+
 // ФУНКЦІЇ
 
 function startNewGame() {
-  currentKeyIndex = Math.floor(Math.random() * keys.length);
   currentKeyIndex = 0;
+  gameStarted = true;
+
   keyEL.textContent = `Натисни клавішу: ${keys[currentKeyIndex]}`;
   stateEL.textContent = 'Гра почалася!';
-
-  alert("Розпочалася нова гра");
-  keyEL.style.marginLeft = '550px'
-
   btnEl.disabled = true;
 }
 
 function onKeydownPress(event) {
+  if (!gameStarted) return;
+
   const pressedKey = event.key.toLowerCase();
   const currentKey = keys[currentKeyIndex];
 
-  if(pressedKey === currentKey){
-    currentKeyIndex += 1
+  if (pressedKey === currentKey) {
+    currentKeyIndex++;
 
-    if(currentKeyIndex === keys.length){
-        stateEL.textContent = 'Гру завершино'
-        btnEl.disabled = false;
-        return
+    if (currentKeyIndex === keys.length) {
+      stateEL.textContent = `Молодець! ${currentKeyIndex}/10
+      Ти пройшов гру!`;
+      btnEl.disabled = false;
+      gameStarted = false;
+      return;
     }
 
     keyEL.textContent = `Натисни клавішу: ${keys[currentKeyIndex]}`;
-
-    stateEL.textContent = "Правильно!";  
-  } else{
-    stateEL.textContent = `Неправельно! Натисни клавішу ${keys[currentKeyIndex]}`;  
+    stateEL.textContent = `Правильно! ${currentKeyIndex}/10`;
+  } else {
+    stateEL.textContent = `Неправильно! Натисни клавішу ${keys[currentKeyIndex]}`;
   }
 }
+
 // ЛІСЕНЕРИ
 
 btnEl.addEventListener("click", startNewGame);
-document.addEventListener('keydown', onKeydownPress)
+document.addEventListener("keydown", onKeydownPress);
